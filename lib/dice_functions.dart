1c7626459f7678fad0645b95c21c224662dc8605
Daniel-Cdemy <daniel_martin@teilnehmer.cdemy.de>
2025-09-30 14:15:44 +0200

    finalize dice_function.dart

--- lib/dice_functions.dart
+++ lib/dice_functions.dart
@@ -5,22 +5,22 @@ int getInt() {
   String intInputString = '';
   int? intInput;
   do {
-    print("Bitte geben Sie eine Zahl zwischen 1 und 10.000 ein:");
+    print("Bitte geben Sie eine Zahl zwischen 3 und 10.000 ein:");
     intInputString = stdin.readLineSync(encoding: utf8) ?? '';
     bool onlyNumbers = RegExp(r'^[0-9]+$').hasMatch(intInputString);
     intInput = int.tryParse(intInputString);
     if (intInputString.isEmpty ||
         !onlyNumbers ||
         intInput == null ||
-        intInput < 1 ||
+        intInput < 3 ||
         intInput > 10000) {
       print(
-        "Das Eingabefeld darf nicht leer sein und muss eine Zahl zwischen 1 und 10.000 sein",
+        "Das Eingabefeld darf nicht leer sein und muss eine Zahl zwischen 3 und 10.000 sein",
       );
     }
   } while (intInputString.isEmpty ||
       intInput == null ||
-      intInput < 1 ||
+      intInput < 3 ||
       intInput > 10000);
   return intInput;
 }
